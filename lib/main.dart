954657c33526bb2ec3883351284739d478d5a052
denuka007 <denukau2001@gmail.com>
2023-01-14 11:56:19 +0530

    myapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
+// import the material package
 import 'package:flutter/material.dart';
-import 'package:quiz_app01/views/home_screen.dart';
 
+import './screens/home_screen.dart'; // the file we just created.
+
+// run the main method
 void main() {
-  runApp(const MyApp());
+  // the runAPp method
+  runApp(
+    const MyApp(), // we will create this below
+  );
 }
 
+// create the MyApp widget
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      // remove the debug banner
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      // set a homepage
+      home: HomeScreen(), // we will create this in separate file.
     );
   }
 }
--- lib/widgets/question_widget.dart
+++ lib/widgets/question_widget.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
+import '../constants.dart'; // it contains our colors.
 
 class QuestionWidget extends StatelessWidget {
   const QuestionWidget(
-      {super.key,
+      {Key? key,
       required this.question,
       required this.indexAction,
-      required this.totalQuestion});
+      required this.totalQuestions})
+      : super(key: key);
+  // here we need the question title and the total number of questions, and also the index
 
   final String question;
   final int indexAction;
-  final int totalQuestion;
+  final int totalQuestions;
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.centerLeft,
       child: Text(
-        'Question ${indexAction + 1}/$totalQuestion: $question',
-        style: const TextStyle(fontSize: 24, color: Colors.white),
+        'Question ${indexAction + 1}/$totalQuestions: $question',
+        style: const TextStyle(
+          fontSize: 24.0,
+          color: neutral,
+        ),
       ),
     );
   }
